10431cd0868f6999d8c6ebb9be3e5e34eb361a90
ehtibar-samedov <e.semedov230@gmail.com>
2024-01-13 12:55:37 +0300

    Главная страница готова

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-$('.menu-button').on('click', function(){
-    $('.navbar').toggleClass('menu-active');
+// $('.menu-button').on('click', function(){
+//     $('.navbar').toggleClass('menu-active');
+// });
+$(document).ready(function () {
+    function closeMenu() {
+        $('.navbar').removeClass('menu-active');
+        $('.menu-button__icon').show();
+        $('.x-icon').hide();
+    }
+
+    $('.menu-button__icon, .x-icon').on('click', function () {
+        $('.navbar').toggleClass('menu-active');
+        $('.menu-button__icon, .x-icon').toggle();
+    });
+
+    // Добавляем обработчик для закрытия меню при касании вне области меню
+    $(document).on('click', function (event) {
+        if (!$(event.target).closest('.menu-button').length && !$(event.target).closest('.navbar').length) {
+            closeMenu();
+        }
+    });
 });
-
-
-
 
 
 
