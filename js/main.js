493938299a6b484fb9e30ae99cb28846e3c07763
ehtibar-samedov <e.semedov230@gmail.com>
2024-01-14 11:52:35 +0300

    Внес изменения

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // $('.menu-button').on('click', function(){
 //     $('.navbar').toggleClass('menu-active');
 // });
+
 $(document).ready(function () {
     function closeMenu() {
         $('.navbar').removeClass('menu-active');
@@ -8,14 +9,15 @@ $(document).ready(function () {
         $('.x-icon').hide();
     }
 
-    $('.menu-button__icon, .x-icon').on('click', function () {
+    $('.menu-button__icon, .x-icon').on('click', function (e) {
+        e.stopPropagation();
         $('.navbar').toggleClass('menu-active');
         $('.menu-button__icon, .x-icon').toggle();
     });
 
     // Добавляем обработчик для закрытия меню при касании вне области меню
     $(document).on('click', function (event) {
-        if (!$(event.target).closest('.menu-button').length && !$(event.target).closest('.navbar').length) {
+        if ($('.navbar').hasClass('menu-active') && !$(event.target).closest('.menu-button').length && !$(event.target).closest('.navbar').length) {
             closeMenu();
         }
     });
@@ -26,7 +28,6 @@ $(document).ready(function () {
 
 
 // stepper
-
 $(document).ready(function() {
     $(".minus").on("click", function() {
        $(".quantity__number").val(parseInt($(".quantity__number").val())-1);
